52b0a275af2d37f121b7953d0248d644d5a1dd65
Sean Murphy <sean@iamseanmurphy.com>
2024-11-19 14:31:30 +0000

    1.0.4

--- shortcode-elementor/elementor-shortcode.php
+++ shortcode-elementor/elementor-shortcode.php
@@ -1,12 +1,21 @@
 <?php
+
 /*plugin name: Shortcode Elementor
- * Plugin URI:  https://rstheme.com/products/wordpress/elementor
+ * Plugin URI:  https://wordpress.org/plugins/shortcode-elementor/
  * Description: Insert your elementor pages and sections templates anywhere using shortcode
- * Version:     1.0.3
+ * Version:     1.0.4
  * Author:      RSTheme
  * Author URI:  https://rstheme.com/
  * Text Domain: rs-shortcode
+ * Author URI: 	http://rstheme.com
+ * Plugin URI: 	https://wordpress.org/plugins/shortcode-elementor/
+ * License: 	GPL v2 or later
+ * License URI:	http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ * Requires PHP: 7.0.0
+ * Requires at least: 5.5
  */
+
 define( 'RS_Elements__FILE__', __FILE__ );
 define( 'RS_Elements_PLUGIN_BASE', plugin_basename( RS_Elements__FILE__ ) );
 define( 'RS_Elements_URL', plugins_url( '/', RS_Elements__FILE__ ) );
@@ -22,14 +31,46 @@ function rselements_load_textdomain_lite() {
 }
 add_action('plugins_loaded', 'rselements_load_textdomain_lite');
 
+// Admin Notice for Black Friday Deal
+function rselements_admin_notice_lite() {
+    if ( !current_user_can( 'manage_options' ) ) {
+        return;
+    }
+
+    $screen = get_current_screen();
+    if ( $screen->base !== 'dashboard' ) {
+        return;
+    }
+
+    ?>
+    <div class="notice notice-success is-dismissible" style="background: url('<?php echo esc_url(RS_Elements_URL . 'includes/img/admin-notice.png'); ?>') no-repeat center center / cover; color: #fff; padding: 20px; display: flex; align-items: center; justify-content: space-between;">
+        <p style="margin: 0; font-size: 26px; line-height: 1.4; letter-spacing: 1px; font-weight: 700;">
+            <?php esc_html_e( 'Get Up To', 'ultimate-team-showcase' ); ?>
+            <span style="color: #FF0000; font-weight: bold; padding-left: 5px; padding-right: 5px;"><?php esc_html_e( '50% OFF', 'ultimate-team-showcase' ); ?></span>
+            <?php esc_html_e( 'on The Premium WordPress Themes Collection on', 'ultimate-team-showcase' ); ?>
+            <span style="color: #FF0000; font-weight: bold; padding-left: 5px;"><?php esc_html_e( 'Black Friday & Cyber Monday Deal!', 'ultimate-team-showcase' ); ?></span>
+        </p>
+
+        <a href="https://rstheme.com/offers/" class="button rselements-button" style="text-decoration: none; margin-right: 50px; background-color: #fff; color: #000; border-radius: 15px; font-weight: 600; font-size: 20px;">
+            <?php esc_html_e( 'Snag The Deals!', 'ultimate-team-showcase' ); ?>
+        </a>
+
+        <style>
+            .rselements-button:hover {
+                background-color: #ffffff !important;
+                color: #ff0000 !important;
+            }
+        </style>
+    </div>
+    <?php
+}
+add_action( 'admin_notices', 'rselements_admin_notice_lite' );
+
 class RSElements_Elementor_Shortcode{
 
 	function __construct(){
 		add_action( 'manage_rs_elements_posts_custom_column' , array( $this, 'rselements_rs_global_templates_columns' ), 10, 2);
 		add_filter( 'manage_rs_elements_posts_columns', array($this,'rselements_custom_edit_global_templates_posts_columns' ));
-		// Add Upgrade to Pro sub-menu
-        add_action('admin_menu', array($this, 'rs_shortcode_demo_submenu'));	
-
 	}
 
 	function rselements_custom_edit_global_templates_posts_columns($columns) {
@@ -47,30 +88,7 @@ class RSElements_Elementor_Shortcode{
 				echo '<input type=\'text\' class=\'widefat\' value=\'[SHORTCODE_ELEMENTOR id="'.$post_id.'"]\' readonly="">';
 				break;
 		}
-	}
-
-	    // Function to add Upgrade to Pro sub-menu
-    function rs_shortcode_demo_submenu() {
-        add_submenu_page(
-            'edit.php?post_type=rs_elements',
-            'Get Help',
-            'Get Help',
-            'manage_options',
-            'rs_shortcode_demo',
-            array($this, 'rs_shortcode_demo_page')
-        );
-    }
-
-    // Callback function for Upgrade to Pro sub-menu page
-    function rs_shortcode_demo_page() {
-        // Add your Upgrade to Pro page content here
-        echo '<div class="wrap">';
-         require_once( RS_Elements_PATH . 'includes/all-demo.php' );
-        // Add your content here
-        echo '</div>';
-    }
-    
-	
+	}	
 }
 new RSElements_Elementor_Shortcode();
 
